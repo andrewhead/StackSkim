c3e7b2b3015cb66b2766f328731a1944ef3e963b
Andrew Head <head.andrewm@gmail.com>
2015-06-30 17:58:52 -0700

    Evaluations: #39 added TSV for the domains scraped

---
+++ eval/e39/data_to_tsv.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+import logging
+from models import Page
+from urlparse import urlparse
+
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+
+def main():
+    print '\t'.join(['domain', 'language', 'query', 'has_example', 'notfound'])
+    for page in Page.select():
+        print '\t'.join(str(_) for _ in [
+            urlparse(page.link).netloc,
+            page.language,
+            page.query,
+            page.has_example,
+            page.notfound,
+        ])
+
+
+if __name__ == '__main__':
+    main()
