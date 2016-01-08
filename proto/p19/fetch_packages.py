3c0dfda479f1dbc0f61769099205824461a6e4ce
Austin Le <austin.h.le@gmail.com>
2016-01-07 23:12:47 -0800

    Prototypes: #19, remove unused functions and data fields.

--- proto/p19/fetch_packages.py
+++ proto/p19/fetch_packages.py
@@ -5,12 +5,9 @@ from __future__ import unicode_literals
 import logging
 import slumber
 import argparse
-import base64
-import re
 import time
 import requests
 from bs4 import BeautifulSoup
-import json
 
 from peewee import fn
 from models import Package, create_tables
@@ -63,94 +60,6 @@ def make_request(method, *args, **kwargs):
     return res
 
 
-def fetch_packagenames_from_libraryio(package_count):
-    # Currently only fetchers packages in multiples of 30
-    # If package_count == -1, then all packages will be fetched
-
-    keep_fetching = True
-    page_no = 0
-    count_retrieved = 0
-
-    while keep_fetching:
-
-        logging.info("Fetching page %d of package names", page_no)
-        packages = make_request(
-            libraries_io.search.get,
-            q='', platforms='PyPI', page=page_no
-        )
-
-        if packages is not None:
-            for p in packages:
-                Package.get_or_create(
-                    name=p['name'],
-                    repository_url=p['repository_url'],
-                    page_no=page_no,
-                )
-
-        count_retrieved += len(packages)
-        page_no += 1
-        keep_fetching = (
-            (len(packages) > 0) and
-            (package_count == -1 or count_retrieved < package_count)
-        )
-
-
-def github_info(url):
-    if 'github.com' in url:
-        url = re.sub('^.*(?:github.com)', '', url)
-        # Repo name can have slashes, so split on the first two slashes
-        _, user, repo_name = url.split('/', 2)
-        return (user, repo_name)
-    return None
-
-
-def fetch_github_readmes(packages):
-
-    for p in packages:
-
-        gh_info = github_info(p.repository_url)
-        if gh_info is None:
-            logging.info(
-                "Not fetching README.  " +
-                "Package %s does not have a Github repository",
-                p.name)
-            continue
-        user, repo_name = gh_info
-
-        logging.info("Fetching README for %s", p.name)
-        readme = make_request(github.repos(user)(repo_name).readme.get)
-        if readme is not None:
-            p.readme = base64.b64decode(readme['content'])
-            p.save()
-
-
-def fetch_github_stats(packages):
-
-    for p in packages:
-
-        gh_info = github_info(p.repository_url)
-        if gh_info is None:
-            logging.info(
-                "Not fetching stats.  " +
-                "Package %s does not have a Github repository",
-                p.name)
-            continue
-        user, repo_name = gh_info
-
-        logging.info("Fetching stats for package %s", p.name)
-        repo_info = make_request(
-            libraries_io.github(user)(repo_name).get()
-        )
-        if repo_info is not None:
-            p.stargazers_count = repo_info['stargazers_count']
-            p.forks_count = repo_info['forks_count']
-            p.open_issues_count = repo_info['open_issues_count']
-            p.subscribers_count = repo_info['subscribers_count']
-            p.github_contributions_count = repo_info['github_contributions_count']
-            p.has_wiki = repo_info['has_wiki']
-            p.save()
-
-
 def fetch_package_list():
     res = make_request(
         default_requests_session.get,
@@ -163,7 +72,8 @@ def fetch_package_list():
     for row in all_rows:
         # Each row has 2 columns, a name (hyperlinked) and a description.
         link = row.find('a')
-        if link is not None: # Only fetch package if it has a link to its own page.
+        # Only fetch package if it has a link to its own page.
+        if link is not None:
             # Reformat spacing in extracted name.
             package_name = link.text.replace(u'\xa0', ' ')
             Package.get_or_create(name=package_name)
@@ -183,12 +93,11 @@ def fetch_pypi_data(packages):
         if res is not None:
             page = BeautifulSoup(res.content, 'html.parser')
 
-            # Package description is the first italicized <p> tag on a PyPI
-            # package doc.
+            # Package description is the first italicized <p> tag on a PyPI package doc.
             p.description = page.find('p', style="font-style: italic").text
 
-            # README, if it exists, comes after the above italicized package
-            # description and ends with END_OF_README.
+            # README, if it exists, comes after the above italicized package description and ends
+            # with END_OF_README.
 
             # Explicitly decode content into utf-8, rather than default ascii.
             content = res.content.decode('utf-8')
@@ -229,27 +138,6 @@ if __name__ == '__main__':
         action='store_true',
         help="only update existing data (currently only for --pypi-data)"
     )
-    parser.add_argument(
-        '--lib-packages',
-        action='store_true',
-        help="fetch package names from Library.IO"
-    )
-    parser.add_argument(
-        '--lib-package-count',
-        type=int,
-        default=-1,
-        help="how many package names to fetch"
-    )
-    parser.add_argument(
-        '--github-readmes',
-        action='store_true',
-        help="fetch Github READMEs"
-    )
-    parser.add_argument(
-        '--github-stats',
-        action='store_true',
-        help="fetch Github stats"
-    )
     args = parser.parse_args()
 
     if args.package_list:
@@ -261,10 +149,3 @@ if __name__ == '__main__':
         else:
             packages = Package.select().where(Package.readme >> None).order_by(fn.Random())
         fetch_pypi_data(packages)
-    if args.lib_packages:
-        create_tables()
-        fetch_packagenames_from_libraryio(args.lib_package_count)
-    if args.github_readmes:
-        fetch_github_readmes(Package.select())
-    if args.github_stats:
-        fetch_github_stats(Package.select())
